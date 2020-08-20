ea25fcec46885d88213ae2b06166aac6ffcbd6a2
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-08-19 23:11:12 -1000

    test

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# wizard to provide a shadow to your div
+
+
+`npm i wizard --save`
+
+# Options 
+
+1) type => hard | soft 
+
+2) padding => 0 | 1 
---
+++ index.js
@@ -0,0 +1,18 @@
+
+function wizard(options){
+    let images = document.querySelectorAll('.wizard');
+    if(options.type === 'hard'){
+            options.type = '0px';
+    }
+    else
+    options.type = '15px';
+
+    images.forEach(image => {
+        image.getElementsByClassName.boxShadow = `10px 10px ${options.type} 1px rgba(0,0,0,0.12)`;
+        if(options.padding){
+            image.style.padding = '1em';
+        }
+    })
+
+
+}
